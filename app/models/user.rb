792b8d5900b50d65801f906868fcf785278c6a63
h-ucchi <h-ucchi+study@github.com>
2022-02-20 18:01:52 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, uniqueness: true
+  validates :encrypted_password, length: { minimum: 6 }
+  
+  validates :encrypted_password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :encrypted_password, confirmation: true
 end
