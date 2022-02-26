de22b5af04e75901659f97cffc8e491cad8baa2f
h-ucchi <h-ucchi+study@github.com>
2022-02-26 20:55:07 +0900

    「ー（全角ハイフン）」のバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
     validates :nickname
 
     #名前の漢字入力欄のバリデーション設定
-    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角のみが使えます" } do
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "全角のみが使えます" } do
       validates :last_name
       validates :first_name
     end
@@ -26,5 +26,5 @@ class User < ApplicationRecord
 
   #パスワードの半角英数字のバリデーション設定
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "半角英数字のみが使えます" }
-  
+
 end
