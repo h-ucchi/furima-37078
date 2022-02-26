06527329aa893ee0a7925231ef8616afd1211cbf
h-ucchi <h-ucchi+study@github.com>
2022-02-26 12:15:32 +0900

    バリデーションの更新（2重メッセージの削除）

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   #全体に空白エラーを表示させる
+  #emailとパスワードはdeviseにバリデーション（空欄、パスワードの文字数（6文字））がデフォルトであるので、追記する必要はない
   with_options presence: true do
     validates :nickname
-    validates :email, uniqueness: true
-    
-    validates :password, length: { minimum: 6, message: "6文字以上で入力してください" }
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "半角英数字のみが使えます" }
-    validates :password, confirmation: true
 
-    validates :last_name
-    validates :first_name
+    #名前の漢字入力欄のバリデーション設定
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角のみが使えます" } do
+      validates :last_name
+      validates :first_name
+    end
 
     validates :birthday
 
@@ -24,4 +23,8 @@ class User < ApplicationRecord
       validates :first_name_katakana
     end
   end
+
+  #パスワードの半角英数字のバリデーション設定
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "半角英数字のみが使えます" }
+  
 end
