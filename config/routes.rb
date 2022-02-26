5645bfe866944c4f8a0273a5a41a20fd8637938a
h-ucchi <h-ucchi+study@github.com>
2022-02-26 21:42:18 +0900

    ルーティングの更新

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index" #localhost:3000の遷移先を指定する（root_pathの遷移先になる）
 
-  resources :users
-  resources :items
+  #resources :users
+  #resources :items
 end
