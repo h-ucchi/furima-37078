67da3175e66814124dd793d6faf524f639c2db56
h-ucchi <h-ucchi+study@github.com>
2022-02-26 19:48:03 +0900

    テストのuserデータの更新

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    last_name {"山田"}
+    last_name_katakana {"ヤマダ"}
+    first_name {"太郎"}
+    first_name_katakana {"タロウ"}
+    birthday {"2000-01-01"}
   end
 end
