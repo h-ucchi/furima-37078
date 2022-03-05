be84aa42ba0c714346d256e317a54925bbad195e
h-ucchi <h-ucchi+study@github.com>
2022-03-05 12:25:22 +0900

    マイグレーションファイルの作成

--- db/migrate/20220305031514_create_items.rb
+++ db/migrate/20220305031514_create_items.rb
@@ -1,6 +1,14 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
+      t.string :title,                 null: false, default: ""
+      t.text :description,             null: false, default: ""
+      t.integer :category_id,          null: false, default: ""
+      t.integer :item_condition_id,    null: false, default: ""
+      t.integer :area_id,              null: false, default: ""
+      t.integer :price,                null: false, default: ""
+      t.integer :delivery_cost_id,     null: false, default: ""
+      t.references :user,              null: false, foreign_key: true
 
       t.timestamps
     end
