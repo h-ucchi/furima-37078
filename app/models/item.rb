fb4b192a08ffefbb63852a95cabd92e0d052565e
h-ucchi <h-ucchi+study@github.com>
2022-03-05 13:08:40 +0900

    categoryの選択ボックス作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,7 @@ class Item < ApplicationRecord
     validates :price
     validates :delivery_cost_id
   end
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
+
 end
