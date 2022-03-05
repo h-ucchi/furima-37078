ce0f6f635f5fc3d2f2e9c76d850f5a7544d45bf9
h-ucchi <h-ucchi+study@github.com>
2022-03-05 12:45:51 +0900

    マイグレーションファイルの更新

--- db/migrate/20220305031514_create_items.rb
+++ db/migrate/20220305031514_create_items.rb
@@ -2,12 +2,12 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
       t.string :title,                 null: false, default: ""
-      t.text :description,             null: false, default: ""
-      t.integer :category_id,          null: false, default: ""
-      t.integer :item_condition_id,    null: false, default: ""
-      t.integer :area_id,              null: false, default: ""
-      t.integer :price,                null: false, default: ""
-      t.integer :delivery_cost_id,     null: false, default: ""
+      t.text :description,             null: false #MySQLでは、textのdefault値の設定ができない
+      t.integer :category_id,          null: false
+      t.integer :item_condition_id,    null: false
+      t.integer :area_id,              null: false
+      t.integer :price,                null: false
+      t.integer :delivery_cost_id,     null: false
       t.references :user,              null: false, foreign_key: true
 
       t.timestamps
