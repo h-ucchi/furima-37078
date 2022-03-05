2092361440213dc21dc98741ed95e6debb606674
h-ucchi <h-ucchi+study@github.com>
2022-03-05 12:56:39 +0900

    バリデーションの設定（presence true）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :title
+    validates :description
+    validates :category_id
+    validates :item_condition_id
+    validates :area_id
+    validates :delivery_day
+    validates :price
+    validates :delivery_cost_id
+  end
 end
