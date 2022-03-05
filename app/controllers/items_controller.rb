e83bd9ab4a39ec23687fd71325952771b8cd0008
h-ucchi <h-ucchi+study@github.com>
2022-03-05 15:34:50 +0900

    カテゴリープルダウンリストの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,27 @@
 class ItemsController < ApplicationController
+
   def index
+    @items = Item.order("created_at DESC")
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(category_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def category_params
+    params.require(:item).permit(:title,:text,:category_id)
+  end
+
 end
