a61bda98a46bbaf157cdb07f2497e26fe2a7d34a
h-ucchi <h-ucchi+study@github.com>
2022-03-06 14:13:42 +0900

    テスト追加（ActiveHashを使用するカラム、価格が数字以外）

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -44,15 +44,52 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
+      it 'カテゴリーに「---」が選択されている場合は出品できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+
+      it '商品の状態が空では登録できない' do
+        @item.item_condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item condition can't be blank")
+      end
+      it '商品の状態に「---」が選択されている場合は出品できない' do
+        @item.item_condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item condition can't be blank")
+      end
+
+      it '発送料の負担が空では登録できない' do
+        @item.delivery_cost_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery cost can't be blank")
+      end
+      it '配送料の負担に「---」が選択されている場合は出品できない' do
+        @item.delivery_cost_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery cost can't be blank")
+      end
 
       it '発送元の地域が空では登録できない' do
         @item.area_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Area can't be blank")
       end
+      it '発送元の地域に「---」が選択されている場合は出品できない' do
+        @item.area_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area can't be blank")
+      end
 
       it '発送までの日数が空では登録できない' do
         @item.delivery_day_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery day can't be blank")
+      end
+      it '発送までの日数に「---」が選択されている場合は出品できない' do
+        @item.delivery_day_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery day can't be blank")
       end
@@ -71,6 +108,11 @@ RSpec.describe Item, type: :model do
         @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price は300円から9,999,999円までの金額を登録できます')
+      end
+      it '価格に半角数字以外が含まれている場合は出品できない' do
+        @item.price = 'aaa'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price は300円から9,999,999円までの金額を登録できます")
       end
 
       it 'ユーザーが紐付いていなければ投稿できない' do
