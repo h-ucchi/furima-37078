3c333f56d3ada0432086d6b4682b0fbcc7733480
h-ucchi <h-ucchi+study@github.com>
2022-03-12 16:09:54 +0900

    orderルーティング更新

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index" #localhost:3000の遷移先を指定する（root_pathの遷移先になる）
   resources :items
-  resources :orders, only:[:create]
+  resources :orders
   #resources :users
 end
