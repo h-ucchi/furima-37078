fe9f9cfc787a9bc37f49ae4d9aae8e6998d590b3
h-ucchi <h-ucchi+study@github.com>
2022-03-12 09:19:57 +0900

    destroyをbeforeactionに追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :move_to_user_session, only: [:new, :edit]
+  before_action :move_to_user_session, only: [:new, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy] #before_actionにも順番がある
-  before_action :move_to_root, only: [:edit]
+  before_action :move_to_root, only: [:edit, :destroy]
   
   def index
     @items = Item.all.order(created_at: :desc)
