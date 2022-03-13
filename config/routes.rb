8819c87e88a03cd5325a93844da998ef1a5d7b1a
h-ucchi <h-ucchi+study@github.com>
2022-03-13 12:25:27 +0900

    ルーティングのネスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index" #localhost:3000の遷移先を指定する（root_pathの遷移先になる）
-  resources :items
-  resources :orders
+  resources :items do
+    resources :orders
+  end
   #resources :users
 end
