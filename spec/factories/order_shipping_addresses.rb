798ed2deb60365cbfa9f50735ba6574f10bc2c29
h-ucchi <h-ucchi+study@github.com>
2022-03-13 10:16:47 +0900

    テスト用データの生成

--- spec/factories/order_shipping_addresses.rb
+++ spec/factories/order_shipping_addresses.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :order_shipping_address do
+    post_code { '123-4567' }
+    area { 2 }
+    municipality { '東京都' }
+    address { '1-1' }
+    building_name { '東京ハイツ' }
+    price { 2000 }
     
   end
 end
