857921757ef2dfdca8fc4be3ea3ff8e6face38af
h-ucchi <h-ucchi+study@github.com>
2022-03-13 10:05:18 +0900

    購入ファイルへのバリデーション追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,10 @@
 class Order < ApplicationRecord
   attr_accessor :token
+  
+  belongs_to :user
+  belongs_to :item
+  has_one :shipping_adress
+
   validates :price, presence: true
   validates :token, presence: true
 end
