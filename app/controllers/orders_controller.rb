d5e456e8242f2ed8b997978b381f1d82625cc3a3
h-ucchi <h-ucchi+study@github.com>
2022-03-19 16:37:30 +0900

    orderコントローラーの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,16 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create] #before_actionにも順番がある
+  before_action :move_to_root, only: [:create]
 
   def index
     @orders = Order.all
     @item = Item.find(params[:item_id])
-  end
-
-  def new
     @order_shipping_address = OrderShippingAddress.new
   end
 
   def create
     @order_shipping_address = OrderShippingAddress.new(order_params)
-    #@order = Order.new(order_params)
+    @item = Item.find(params[:item_id])
     if @order_shipping_address.valid?
       pay_item
       @order_shipping_address.save
@@ -24,16 +23,28 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_shipping_address).permit(:price, :number, :exp_month, :exp_year, :cvc, :post_code, :area_id, :municipality, :address, :building_name, :tell_number).merge(token: params[:token], user_id: current_user.id )
+    #住所・トークンの情報をparamsに入れたいので、requireで住所変数を呼び出して、変数の中の情報をpermitで許可してあげる
+    #トークンの情報（≒カード情報）は"token: params[]"でcard.jsから持ってこれている
+    params.require(:order_shipping_address).permit(:post_code, :area_id, :municipality, :address, :building_name, :tell_number).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id] )
   end
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"] #秘密鍵を環境変数に置き換えた
     Payjp::Charge.create(
-      amount: order_params[:price],  # 商品の値段
+      amount: @item.price,           # 商品の値段
       card: order_params[:token],    # カードトークン
-      currency: 'jpy'                 # 通貨の種類（日本円）
+      currency: 'jpy'                # 通貨の種類（日本円）
     )
+  end
+  
+  def set_item 
+    @item = Item.find(params[:item_id])
+  end
+  
+  def move_to_root
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
 end
