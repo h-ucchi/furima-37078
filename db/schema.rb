20a46dcf26e7c2b4cdcbf4b3c0f056258cef3cee
h-ucchi <h-ucchi+study@github.com>
2022-03-19 15:03:25 +0900

    orderモデルへのカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -50,8 +50,12 @@ ActiveRecord::Schema.define(version: 2022_03_12_090949) do
 
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_orders_on_item_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "shipping_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -87,5 +91,7 @@ ActiveRecord::Schema.define(version: 2022_03_12_090949) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
+  add_foreign_key "orders", "items"
+  add_foreign_key "orders", "users"
   add_foreign_key "shipping_addresses", "orders"
 end
