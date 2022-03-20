7ef931adc6c09e2a5c4cdc792432d5ec555ff259
h-ucchi <h-ucchi+study@github.com>
2022-03-20 13:54:35 +0900

    ユニットテスト

--- spec/models/order_shipping_address_spec.rb
+++ spec/models/order_shipping_address_spec.rb
@@ -4,9 +4,14 @@ RSpec.describe OrderShippingAddress, type: :model do
   describe '購入情報の保存' do
     before do
       user = FactoryBot.create(:user)
-      @order_shipping_address = FactoryBot.build(:order_shipping_address, user_id: user.id)
+      item = FactoryBot.create(:item)
+      #order = FactoryBot.create(:order)
+      @order_shipping_address = FactoryBot.build(:order_shipping_address, user_id: user.id, item_id: item.id)
+      sleep(0.5)
     end
 
+    # token情報を入れる必要があるが、orderファイルをBeforeActionで生成するとエラーになる。
+    # 多分ファイルの並び順的にorderの方が上にあるからだと思う
     context '内容に問題ない場合' do
       it 'すべての値が正しく入力されていれば保存できること' do
         expect(@order_shipping_address).to be_valid
@@ -25,15 +30,15 @@ RSpec.describe OrderShippingAddress, type: :model do
         expect(@order_shipping_address.errors.full_messages).to include("Post code can't be blank")
       end
       it 'post_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
-        @order_shipping_address.postal_code = '1234567'
+        @order_shipping_address.post_code = '1234567'
         @order_shipping_address.valid?
         expect(@order_shipping_address.errors.full_messages).to include('Post code is invalid. Include hyphen(-)')
       end
       
       it 'areaを選択していないと保存できないこと' do
-        @order_shipping_address.area = '1'
+        @order_shipping_address.area_id = '1'
         @order_shipping_address.valid?
-        expect(@order_shipping_address.errors.full_messages).to include("Post code can't be blank")
+        expect(@order_shipping_address.errors.full_messages).to include("Area can't be blank")
       end
       
       it 'municipalityを入力していないと保存できないこと' do
@@ -48,32 +53,16 @@ RSpec.describe OrderShippingAddress, type: :model do
         expect(@order_shipping_address.errors.full_messages).to include("Address can't be blank")
       end
 
-      it 'priceが空だと保存できないこと' do
-        @order_shipping_address.price = ''
-        @order_shipping_address.valid?
-        expect(@order_shipping_address.errors.full_messages).to include("Price code can't be blank")
-      end
-      
-      it 'priceが全角数字だと保存できないこと' do
-        @order_shipping_address.price = '２０００'
-        @order_shipping_address.valid?
-        expect(@order_shipping_address.errors.full_messages).to include('Price is invalid')
-      end
-      it 'priceが300円未満では保存できないこと' do
-        @order_shipping_address.price = '299'
-        @order_shipping_address.valid?
-        expect(@order_shipping_address.errors.full_messages).to include('Price is invalid')
-      end
-      it 'priceが10,000,000円より大きい場合は登録できない' do
-        @order_shipping_address.price = '10000000'
-        @order_shipping_address.valid?
-        expect(@order_shipping_address.errors.full_messages).to include('Price is invalid')
-      end
-      
       it 'userが紐付いていないと保存できないこと' do
         @order_shipping_address.user_id = nil
         @order_shipping_address.valid?
         expect(@order_shipping_address.errors.full_messages).to include("User can't be blank")
+      end
+
+      it 'itemが紐付いていないと保存できないこと' do
+        @order_shipping_address.item_id = nil
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include("Item can't be blank")
       end
     end
   end
