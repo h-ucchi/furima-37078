a7c63aa41a9f8008465d7c8a29a206f11a1582ee
h-ucchi <h-ucchi+study@github.com>
2022-03-20 14:16:48 +0900

    factorybotにトークン情報を追加

--- spec/factories/order_shipping_addresses.rb
+++ spec/factories/order_shipping_addresses.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :order_shipping_address do
+    token {"tok_abcdefghijk00000000000000000"}
     post_code { '123-4567' }
     area_id { 2 }
     municipality { '江東区' }
