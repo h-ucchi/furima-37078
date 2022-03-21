de1d55f33a83feeae71c27bca8fa65b9af2e3115
h-ucchi <h-ucchi+study@github.com>
2022-03-21 20:26:05 +0900

    onlyオプションの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index" #localhost:3000の遷移先を指定する（root_pathの遷移先になる）
   resources :items do
-    resources :orders
+    resources :orders, only: [:index, :create]
   end
   #resources :users
 end
